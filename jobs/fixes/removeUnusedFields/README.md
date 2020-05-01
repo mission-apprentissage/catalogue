9495ab44440234cc61a947ff629949c14afbeb2e
Gethi <bigard.antoine@gmail.com>
2020-05-01 06:59:58 +0200

    Add removeUnusedField fixe

---
+++ jobs/fixes/removeUnusedFields/README.md
@@ -0,0 +1,4 @@
+# Remove intitule field
+
+Suppression du champ "intitule" inutile sur les formations
+Champs remplacé par intitule_long et intitule_court
---
+++ jobs/fixes/removeUnusedFields/src/index.js
@@ -0,0 +1,37 @@
+// #region Imports
+const { connectToMongo } = require("../../../common/mongo");
+const { getElasticInstance } = require("../../../common/esClient");
+const logger = require("../../../common/Logger").mainLogger;
+
+const { Formation } = require("../../../common/models");
+
+const run = async () => {
+  try {
+    logger.info(" -- Start Remove Unused Fields -- ");
+    await connectToMongo();
+
+    // Remove initule fields
+    await Formation.collection.updateMany({}, { $unset: { intitule: "" } });
+    await rebuildIndex("formations", Formation);
+
+    // Remove siren fields
+    await Formation.collection.updateMany({}, { $unset: { siren: "" } });
+    await rebuildIndex("formations", Formation);
+
+    logger.info(" -- End Remove Unused Fields -- ");
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+const rebuildIndex = async (index, schema) => {
+  let client = getElasticInstance();
+
+  logger.info(`Removing '${index}' index...`);
+  await client.indices.delete({ index });
+
+  logger.info(`Rebuilding '${index}' index...`);
+  await schema.synchronize();
+};
+
+run();
