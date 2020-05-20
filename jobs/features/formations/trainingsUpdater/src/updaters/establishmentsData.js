973d7254af50bf7ec85bc55d542c353455bb143e
Gethi <bigard.antoine@gmail.com>
2020-05-20 14:23:22 +0200

    Add raison sociale

--- jobs/features/formations/trainingsUpdater/src/updaters/establishmentsData.js
+++ jobs/features/formations/trainingsUpdater/src/updaters/establishmentsData.js
@@ -34,6 +34,7 @@ class EstablishmentsData {
     }
 
     return {
+      entreprise_raison_sociale: etablissement_reference.entreprise_raison_sociale,
       etablissement_responsable_published: attachedEstablishments.responsable
         ? attachedEstablishments.responsable.published
         : false,
