5842dd28ce7fab9b0e9127bc09ba69c8437978cc
Alan Le Ruyet <alan.leruyet@free.fr>
2020-05-25 10:08:55 +0200

    Etablissement formateur loc -> Etablissement référence localisation

--- common/models/trainingSchema.js
+++ common/models/trainingSchema.js
@@ -49,21 +49,6 @@ const trainingSchema = {
     default: false,
     description: "Etablissement formateur entre dans le catalogue",
   },
-  etablissement_formateur_localisation_coordonnees_lon: {
-    type: Number,
-    default: 0,
-    description: "Localisation coordonnees Longitude de l'établissement formateur",
-  },
-  etablissement_formateur_localisation_coordonnees_lat: {
-    type: Number,
-    default: 0,
-    description: "Localisation coordonnees Latitude de l'établissement formateur",
-  },
-  etablissement_formateur_localisation_geojson: {
-    type: Object,
-    default: {},
-    description: "Localisation GeoJson de l'établissement formateur",
-  },
   etablissement_responsable_id: {
     type: Object,
     default: {},
@@ -154,6 +139,21 @@ const trainingSchema = {
     type: Boolean,
     default: false,
     description: "Etablissement reference est publié",
+  },
+  etablissement_reference_localisation_coordonnees_lon: {
+    type: Number,
+    default: 0,
+    description: "Localisation coordonnees Longitude de l'établissement de référence",
+  },
+  etablissement_reference_localisation_coordonnees_lat: {
+    type: Number,
+    default: 0,
+    description: "Localisation coordonnees Latitude de l'établissement de référence",
+  },
+  etablissement_reference_localisation_geojson: {
+    type: Object,
+    default: {},
+    description: "Localisation GeoJson de l'établissement de référence",
   },
   entreprise_raison_sociale: {
     type: String,
