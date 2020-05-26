ab389397e7b4b756ab707b97985ec97dcb003d43
Alan Le Ruyet <alan.leruyet@free.fr>
2020-05-26 16:15:48 +0200

    Ecriture de localisation_gson pour établissement format geopoint

--- jobs/features/etablissements/etablishmentsUpdater/src/updaters/geoData.js
+++ jobs/features/etablissements/etablishmentsUpdater/src/updaters/geoData.js
@@ -10,17 +10,21 @@ class GeoData {
       return false;
     }
     const geojson = { ...responseApiAdresse };
-    delete geojson.limit;
+    /*delete geojson.limit; delete rendus inutiles car pas de stockage de la données complète dans mongo
     delete geojson.filters;
     delete geojson.query;
     delete geojson.licence;
     delete geojson.attribution;
-    delete geojson.version;
+    delete geojson.version;*/
 
     return {
-      localisation_geojson: geojson,
+      /*localisation_geojson: geojson,
       localisation_coordonnees_lon: geojson.features[0].geometry.coordinates[0],
-      localisation_coordonnees_lat: geojson.features[0].geometry.coordinates[1],
+      localisation_coordonnees_lat: geojson.features[0].geometry.coordinates[1],*/
+      localisation_geojson: {
+        lon: geojson.features[0].geometry.coordinates[0],
+        lat: geojson.features[0].geometry.coordinates[1],
+      },
     };
   }
 }
