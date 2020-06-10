db01413eb7054f73b5187631a0fad07515c1bc0d
Gethi <bigard.antoine@gmail.com>
2020-06-10 16:02:08 +0200

    .

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -44,7 +44,7 @@ module.exports.handler = async (event, context, callback) => {
           if (id === "") {
             throw new Error("Something went wrong: id missing");
           }
-          // console.log(await execShellCommand(`ls -l`));
+          console.log(await execShellCommand(`ls -l`));
           // eslint-disable-next-line no-case-declarations
           const { run } = require(`${basePath}/jobs/features/formations/trainingsUpdater/src/trainingsUpdater`);
           await run({ _id: id });
