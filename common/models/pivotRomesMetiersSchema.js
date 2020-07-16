4f8e49eb5d3d5472c0d0547cb5db995b4877d24f
Alan Le Ruyet <alan.leruyet@free.fr>
2020-07-16 15:25:53 +0200

    Ajout champs

--- common/models/pivotRomesMetiersSchema.js
+++ common/models/pivotRomesMetiersSchema.js
@@ -4,6 +4,46 @@ const pivotRomesMetiersSchema = {
     default: null,
     description: "Le sous-domaine d'un métier",
   },
+  domaine: {
+    type: String,
+    default: null,
+    description: "Le grand domaine d'un métier",
+  },
+  domaines: {
+    type: [String],
+    default: [],
+    description: "Les domaines d'un métier",
+  },
+  familles: {
+    type: [String],
+    default: [],
+    description: "Les familles associées au métier",
+  },
+  codes_romes: {
+    type: [String],
+    default: [],
+    description: "Les codes Romes associés au métier",
+  },
+  intitules_romes: {
+    type: [String],
+    default: [],
+    description: "Les libellés des codes ROMEs associés au métier",
+  },
+  /*codes_rncps: {
+    type: [String],
+    default: [],
+    description: "Les codes RNCPs associés au métier",
+  },*/
+  mots_clefs: {
+    type: String,
+    default: null,
+    description: "Les mots clefs associés au métier",
+  },
+  couples_romes_metiers: {
+    type: [Object],
+    default: [],
+    description: "Couples codes ROMEs / intitulés correspondants au métier",
+  }
 };
 
 module.exports = pivotRomesMetiersSchema;
