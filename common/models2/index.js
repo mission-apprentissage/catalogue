af2b3d88fb15fa99826f82519f3f93260808f883
Alan Le Ruyet <alan.leruyet@free.fr>
2020-07-16 09:34:22 +0200

    Ajout de RomesMetiers aux modèles

--- common/models2/index.js
+++ common/models2/index.js
@@ -1,7 +1,7 @@
 const { mongooseInstance: mongooseInst } = require("../mongo");
 const { mongoosastic, getElasticInstance } = require("../esClient");
 
-const { establishmentSchema, trainingSchema } = require("../models");
+const { establishmentSchema, trainingSchema, pivotRomesMetiersSchema } = require("../models");
 
 const getModel = (MODELNAME, schema, mongooseInstance = mongooseInst, stage = null) => {
   const Schema = new mongooseInstance.Schema(schema);
@@ -20,9 +20,16 @@ if (!f) {
   f = getModel("formations", trainingSchema);
 }
 
+let pRM = null;
+if (!pRM) {
+  pRM = getModel("pivotRomesMetiers", pivotRomesMetiersSchema);
+}
+
 module.exports = {
   Establishment: e,
   Formation: f,
+  RomesMetiers: pRM,
   attachFormationTo: (minst, stage) => getModel("formations", trainingSchema, minst, stage),
   attachEstablishmentTo: (minst, stage) => getModel("etablissements", establishmentSchema, minst, stage),
+  attachRomesMetiersTo: (minst, stage) => getModel("pivotRomesMetiers", pivotRomesMetiersSchema, minst, stage),
 };
