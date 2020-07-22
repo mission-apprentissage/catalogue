efe18f89594294d21a7e1a7805173702238ae051
Alan Le Ruyet <alan.leruyet@free.fr>
2020-07-22 16:40:16 +0200

    Nouvelle version de l'alog parcourant tous les onglets du fichier source
    Chaque onglet correspondant aux métiers d'une lettre
    Chaque bloc de données jusqu'à une ligne avec une info dans la colonne isSousDomaine correspondant à une entrée dans la db

--- jobs/features/domainesMetiers/src/initDomainesMetiers.js
+++ jobs/features/domainesMetiers/src/initDomainesMetiers.js
@@ -16,31 +16,87 @@ const run = async () => {
       return { sheet_name_list: workbook.SheetNames, workbook };
     };
 
-    const fichierDomainesMetiers = "./assets/domainesMetiers.ods";
+    const fichierDomainesMetiers = "./assets/domainesMetiers.xlsx";
     const workbookDomainesMetiers = readXLSXFile(fichierDomainesMetiers);
 
-    let domainesMetiers = XLSX.utils.sheet_to_json(
-      workbookDomainesMetiers.workbook.Sheets[workbookDomainesMetiers.sheet_name_list[0]]
-    );
+    let domaines, familles, codesROMEs, intitulesROMEs, couplesROMEsIntitules;
 
-    for (let i = 0, l = domainesMetiers.length; i < l; ++i) {
-      let domainesMetier = new DomainesMetiers({
-        ...domainesMetiers[i],
-        domaines: domainesMetiers[i].domaines.split(",").map(item => item.trim()),
-        familles: domainesMetiers[i].familles.split(",").map(item => item.trim()),
-        codes_romes: domainesMetiers[i].codes_romes.split(", "),
-        intitules_romes: JSON.parse(domainesMetiers[i].intitules_romes),
-        couples_romes_metiers: JSON.parse(domainesMetiers[i].couples_romes_metiers),
-      });
+    const reset = () => {
+      domaines = [];
+      familles = [];
+      codesROMEs = [];
+      intitulesROMEs = [];
+      couplesROMEsIntitules = [];
+    };
 
-      await domainesMetier.save();
+    for (let i = 0; i < workbookDomainesMetiers.sheet_name_list.length; ++i) {
+      logger.info(`Début traitement lettre : ${workbookDomainesMetiers.sheet_name_list[i]}`);
 
-      logger.info(`Added ${domainesMetier._id} to collection `);
+      let onglet = XLSX.utils.sheet_to_json(
+        workbookDomainesMetiers.workbook.Sheets[workbookDomainesMetiers.sheet_name_list[i]]
+      );
 
-      //console.log(romesMetier);
+      reset();
+
+      //console.log(onglet[0]);
+      for (let j = 0; j < onglet.length; j++) {
+        //console.log(onglet[j]);
+
+        if (onglet[j].isSousDomaine) {
+          // cas de la ligne sur laquelle se trouve le sous-domaine qui va marquer l'insertion d'une ligne dans la db
+          //console.log(onglet[j].isSousDomaine);
+
+          // récupéraration des dernières data, construction de l'objet et sauvegarde
+
+          //console.log("domaines : ",domaines);
+          //console.log("codesROMEs : ",codesROMEs);
+          //console.log("familles : ",familles);
+          //console.log("intitulesROMEs : ", intitulesROMEs);
+          //console.log("couplesROMEsIntitules : ", couplesROMEsIntitules);
+
+          let domainesMetier = new DomainesMetiers({
+            domaine: onglet[j]["Domaine "], // haha, vous l'avez vu cet espace à la fin ? :)
+            sous_domaine: onglet[j]["Sous domaine "], // et celui là ?
+            mots_clefs: onglet[j]["mots clés"],
+            domaines: domaines,
+            familles: familles,
+            codes_romes: codesROMEs,
+            intitules_romes: intitulesROMEs,
+            couples_romes_metiers: couplesROMEsIntitules,
+          });
+
+          //console.log("domainesMetier  : ",domainesMetier);
+          await domainesMetier.save();
+
+          logger.info(`Added ${domainesMetier.sous_domaine} ${domainesMetier._id} to collection `);
+
+          reset();
+        } else {
+          if (onglet[j].Domaine && domaines.indexOf(onglet[j].Domaine.trim()) < 0)
+            domaines.push(onglet[j].Domaine.trim());
+          if (onglet[j].Famille && familles.indexOf(onglet[j].Famille.trim()) < 0)
+            familles.push(onglet[j].Famille.trim());
+
+          //couplesROMEsIntitules
+          if (
+            (onglet[j]["Codes ROME"] &&
+              onglet[j]["Intitulé code ROME"] &&
+              codesROMEs.indexOf(onglet[j]["Codes ROME"].trim()) < 0) ||
+            intitulesROMEs.indexOf(onglet[j]["Intitulé code ROME"].trim()) < 0
+          ) {
+            couplesROMEsIntitules.push({
+              codeRome: onglet[j]["Codes ROME"].trim(),
+              intitule: onglet[j]["Intitulé code ROME"].trim(),
+            });
+          }
+
+          if (onglet[j]["Codes ROME"] && codesROMEs.indexOf(onglet[j]["Codes ROME"].trim()) < 0)
+            codesROMEs.push(onglet[j]["Codes ROME"].trim());
+          if (onglet[j]["Intitulé code ROME"] && intitulesROMEs.indexOf(onglet[j]["Intitulé code ROME"].trim()) < 0)
+            intitulesROMEs.push(onglet[j]["Intitulé code ROME"].trim());
+        }
+      }
     }
-
-    logger.info(" -- End of DomainesMetiers initializer -- ");
   } catch (err) {
     logger.error(err);
   }
