e5ca151aa42b331983f0e4f3b8464d3d8b921ab6
Gethi <bigard.antoine@gmail.com>
2020-07-23 14:46:53 +0200

    update duplicates

--- jobs/features/formations/DuplicateHandler/src/index.js
+++ jobs/features/formations/DuplicateHandler/src/index.js
@@ -20,7 +20,7 @@ const { Formation } = require("../../../../common-jobs/models");
 
 const NB_THREAD = 64;
 const attrToCompare = [
-  //"etablissement_formateur_siret",
+  "etablissement_formateur_siret",
   "etablissement_responsable_siret",
   //"diplome",
   "educ_nat_code",
