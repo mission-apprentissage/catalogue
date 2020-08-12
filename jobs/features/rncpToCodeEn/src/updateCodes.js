79b6499d4c1a351458c947dae2efa2d6f4c1274b
Samir <sbenfares@gmail.com>
2020-08-12 10:40:26 +0200

    Fix stats & package.json

--- jobs/features/rncpToCodeEn/src/updateCodes.js
+++ jobs/features/rncpToCodeEn/src/updateCodes.js
@@ -7,8 +7,16 @@ module.exports = async (codesDiplomesFile, options = {}) => {
   let referentiel = createReferentiel();
   let stats = {
     formations: {
-      updatedRncpFromCodeEn: 0,
-      updatedCodeEnFromRncp: 0,
+      updateRncp: {
+        updated: 0,
+        notFound: 0,
+      },
+
+      updateCodeEn: {
+        updated: 0,
+        notFound: 0,
+      },
+
       errors: 0,
     },
     referentiel: 0,
@@ -22,31 +30,41 @@ module.exports = async (codesDiplomesFile, options = {}) => {
     Formation.find(options.query ? options.query : {}).cursor(),
     writeObject(
       async f => {
-        const updateMode = options.updateMode ? options.updateMode : "findCodeEn";
+        const updateMode = options.updateMode ? options.updateMode : "updateCodeRncpFromCodeEn";
 
         try {
-          if (updateMode === "findCodeEn") {
+          if (updateMode === "updateCodeEnFromCodeRncp") {
             if (f.rncp_code) {
               if (!f.educ_nat_code || (f.educ_nat_code && options.overrideMode)) {
                 let educ_nat_code = referentiel.findCodeEn(f.rncp_code);
                 f.educ_nat_code = educ_nat_code;
-                logger.debug(
-                  `Updating formation ${f._id} : educ_nat_code updated to ${f.educ_nat_code} with rncp_code ${f.rncp_code} found`
-                );
-                await f.save();
-                stats.formations.updatedCodeEnFromRncp++;
+                if (f.educ_nat_code) {
+                  logger.debug(
+                    `Formation ${f._id} : educ_nat_code updated to ${f.educ_nat_code} with rncp_code ${f.rncp_code} found`
+                  );
+                  await f.save();
+                  stats.formations.updateCodeEn.updated++;
+                } else {
+                  logger.debug(`Formation ${f._id} : no educ_nat_code found for rncp_code ${f.rncp_code}`);
+                  stats.formations.updateCodeEn.notFound++;
+                }
               }
             }
-          } else if (updateMode === "findCodeRNCP") {
+          } else if (updateMode === "updateCodeRncpFromCodeEn") {
             if (f.educ_nat_code) {
               if (!f.rncp_code || (f.rncp_code && options.overrideMode)) {
                 let rncp_code = referentiel.findCodeRNCP(f.educ_nat_code);
-                f.rncp_code = rncp_code;
-                logger.debug(
-                  `Updating formation ${f._id} : rncp_code updated to ${f.rncp_code} with educ_nat_code ${f.educ_nat_code} found`
-                );
-                await f.save();
-                stats.formations.updatedRncpFromCodeEn++;
+                if (rncp_code) {
+                  f.rncp_code = rncp_code;
+                  logger.debug(
+                    `Formation ${f._id} : rncp_code updated to ${f.rncp_code} with educ_nat_code ${f.educ_nat_code} found`
+                  );
+                  await f.save();
+                  stats.formations.updateRncp.updated++;
+                } else {
+                  logger.debug(`Formation ${f._id} : no rncp_code found for educ_nat_code ${f.educ_nat_code}`);
+                  stats.formations.updateRncp.notFound++;
+                }
               }
             }
           }
