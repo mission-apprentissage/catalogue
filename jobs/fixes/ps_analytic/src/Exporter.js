13f058565dcecb96dce3977656e28bfdb9487fc2
Kevin Barns <kevbarns@macbook-pro-de-kevin.home>
2020-11-12 18:50:00 +0100

    remove header from xlsx generator

--- jobs/fixes/ps_analytic/src/Exporter.js
+++ jobs/fixes/ps_analytic/src/Exporter.js
@@ -5,29 +5,7 @@ class Exporter {
   constructor() {}
 
   async toXlsx(jsonData, fileName) {
-    const WS = XLSX.utils.json_to_sheet(jsonData, {
-      header: [
-        "UAI_GES",
-        "UAI_COMPOSANTE",
-        "LIB_COMPOSANTE",
-        "LIB_INS",
-        "UAI_AFF",
-        "LIB_AFF",
-        "CODECOMMUNE",
-        "LIBCOMMUNE",
-        "CODEPOSTAL",
-        "ACADÉMIE",
-        "MINISTERETUTELLE",
-        "LIBMINISTERE",
-        "TYPEETA",
-        "CODEFORMATION",
-        "LIBFORMATION",
-        "CODESPÉCIALITÉ",
-        "LIBSPÉCIALITÉ",
-        "CODESPÉFORMATIONINITIALE",
-        "CODEMEF",
-      ],
-    });
+    const WS = XLSX.utils.json_to_sheet(jsonData);
 
     const workbook = XLSX.utils.book_new(); // Create a new blank workbook
     XLSX.utils.book_append_sheet(workbook, WS, "book"); // Add the worksheet to the workbook
